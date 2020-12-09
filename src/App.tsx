132a93bf12a01f3a297bd3bdf311f2368f741521
Paweł Choniawko <pawel.choniawko@gmail.com>
2020-12-09 09:41:50 +0100

    init state of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
 import React from "react"
-import { Logo } from "./Logo"
+import logo from "./logo.svg"
 import "./App.css"
-import { useIterval } from "./useInterval"
 
 function App() {
-  const { state, setState } = useIterval("40")
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo height={`${state}vmin`} color="tomato" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
-        <input
-          value={state}
-          type="range"
-          onChange={(e) => setState(e.target.value)}
-        />
       </header>
     </div>
   )
